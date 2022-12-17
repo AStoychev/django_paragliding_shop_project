2fc053ef7b67fe1f78bb44035054eee50039c833
A.Stoychev <109471350+AStoychev@users.noreply.github.com>
2022-12-17 16:04:02 +0200

    Update admin.py

--- paragliding_shop/equipment/admin.py
+++ paragliding_shop/equipment/admin.py
@@ -7,8 +7,4 @@ from paragliding_shop.equipment.models import Wings, Harness, Reserves, Instrume
 class UnitAdmin(admin.ModelAdmin):
     list_display = ("manufacturer", "price", "condition", "image_url", "user",)
     list_filter = ["manufacturer"]
-
-    def get_readonly_fields(self, request, obj=None):
-        if obj and obj.type == 'manufacturer':
-            return []
-        return ["manufacturer", "model", "price", "condition", "image_url", "user"]
+    
